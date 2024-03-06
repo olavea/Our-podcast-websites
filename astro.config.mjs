d9bb8a9d7e73d67a8f95c3b75774806b02d9f19b
raae <queen@raae.codes>
2024-03-06 14:43:32 +0100

    feat: generate one page per episode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: netlify()
+  output: "static",
+  adapter: netlify(),
 });
