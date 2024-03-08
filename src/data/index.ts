76936487bb2c2f46d5a9d5720aace30df2850c0d
raae <queen@raae.codes>
2024-03-08 14:57:38 +0100

    feat: most popular

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,3 +1,5 @@
+import { format, sub } from "date-fns";
+
 interface Episode {
   slug: string;
   title: string;
@@ -23,6 +25,9 @@ interface TransistorEpisodeResponse {
 const TRANSISTOR_API_KEY = import.meta.env.TRANSISTOR_API_KEY;
 const TRANSISTOR_SHOW_ID = import.meta.env.TRANSISTOR_SHOW_ID;
 const IS_DEV = import.meta.env.DEV;
+const TODAY = new Date();
+const START_DATE = sub(TODAY, { weeks: 53 });
+const END_DATE = sub(TODAY, { weeks: 1 });
 
 export async function fetchEpisodes(
   apiKey = TRANSISTOR_API_KEY,
@@ -60,4 +65,55 @@ export async function fetchEpisodes(
   console.log("Fetched", episodeData.length, "episodes");
 
   return episodeData;
+}
+
+export async function fetchEpisode(episodeId, apiKey = TRANSISTOR_API_KEY) {
+  const response = await fetch(
+    `https://api.transistor.fm/v1/episodes/${episodeId}`,
+    {
+      method: "GET",
+      headers: {
+        "x-api-key": apiKey,
+      },
+    }
+  ).then((response) => response.json());
+  return response.data;
+}
+
+export async function fetchEpisodeAnalytics(
+  apiKey = TRANSISTOR_API_KEY,
+  showId = TRANSISTOR_SHOW_ID,
+  startDate = format(START_DATE, "dd-MM-yyyy"),
+  endDate = format(END_DATE, "dd-MM-yyyy")
+) {
+  const response = await fetch(
+    `https://api.transistor.fm/v1/analytics/${showId}/episodes?start_date=${startDate}&end_date=${endDate}`,
+    {
+      method: "GET",
+      headers: {
+        "x-api-key": apiKey,
+      },
+    }
+  ).then((response) => response.json());
+
+  const analytics = response.data.attributes.episodes.map((episode: any) => {
+    episode.totalDownloads = episode.downloads.reduce(
+      (acc: number, day: any) => acc + day.downloads,
+      0
+    );
+    return episode;
+  });
+
+  return analytics;
+}
+
+export async function fetchMostPopuplarEpisodes(count = 5) {
+  const episodeAnalytics = await fetchEpisodeAnalytics();
+  const sortedEpisodes = episodeAnalytics.sort(
+    (a: any, b: any) => b.totalDownloads - a.totalDownloads
+  );
+  const mostPopular = sortedEpisodes.slice(0, count);
+  return await Promise.all(
+    mostPopular.map((episode: any) => fetchEpisode(episode.id))
+  );
 }
