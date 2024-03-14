1df1ab089a8207191af6c16937fc5b1818f1f947
raae <queen@raae.codes>
2024-03-14 10:38:04 +0100

    feat: configurable title and styling

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,6 +4,8 @@
 interface ImportMetaEnv {
   readonly TRANSISTOR_API_KEY: string;
   readonly TRANSISTOR_SHOW_ID: string;
+  readonly SITE_TITLE: string;
+  readonly SITE_DESCRIPTION: string;
 }
 
 interface ImportMeta {
